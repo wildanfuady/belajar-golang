aa3d807ef6a8f4ba22b5dc8e95a34b824f73f6c8
Ilmu Coding <ilmucoding.com@gmail.com>
2020-04-03 12:35:27 +0700

    part 5

---
+++ part_5_komentar_inline.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Ini contoh komentar inline di Golang
+	// Output
+	fmt.Println("Komentar Inline")
+}
---
+++ part_5_komentar_multiline.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* Ini contoh komentar multiline di Golang
+
+	Anda bisa menyisipkan kode apa saja di sini
+
+	*/
+	fmt.Println("Komentar Multiline")
+}
